44b869492b994e3da7870740e8f4f42c96d0934e
osamabinsaleem <osamabinsaleem@yahoo.com>
2019-12-10 14:16:22 +0500

    initial project version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CreateTodo from './containers/CreateTodo'
+import Table from './containers/Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container" style={{ marginTop: "80px"}} >
+          <div className="row">
+            <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
+              <CreateTodo />
+            </div>
+            <Table />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
